e434a129456609095018502a5506510e324e1212
AbdullahMouzeen <abdullah.mouazeen@gmail.com>
2020-10-28 16:22:15 +0300

    lab 7

--- server.js
+++ server.js
@@ -1,31 +1,60 @@
+'use strict';
 let ex = require('express');
 let app = ex();
 let cors = require('cors');
+let pg = require('pg');
 app.use(cors());
 let superagent = require('superagent');
 
 require('dotenv').config();
+let city0 = 'amman';
 const PORT = process.env.PORT;
 const KEY = process.env.GEOCODE_API_KEY;
-const KEY1 = process.env.Master_API_Key;
-const KEY2 = process.env.Hiking_Key;
+const KEY1 = process.env.MASTER_API_KEY;
+const KEY2 = process.env.HIKING_KEY;
+const KEY3 = process.env.DATABASE_URL;
 
-app.get('/location', handleLocation);
+let client = new pg.Client(KEY3);
+
+//app.get('/location', handleLocation);
 app.get('/weather', handleWeather);
 app.get('/trails', handleTrails);
+
+/*
 
 function handleLocation(req, res) {
     //try {
     let city = req.query.city;
-    superagent.get(`https://us1.locationiq.com/v1/search.php?key=${KEY}&q=${city}&format=json
-            `).then(data => {
-        let jObj = data.body[0];
-        let locObj = new Location(city, jObj.display_name, jObj.lat, jObj.lon);
-        res.status(200).json(locObj);
-    }).catch(() => {
-        res.send('location error')
+    database(city).then(data => {
+        
+        if (data.rowCount > 0) {
+            res.json()
+
+        } else {
+            gitAPI(city);
+
+        }
     });
+
 }
+
+function gitAPI(city ,res ) {
+    // superagent.get(`https://us1.locationiq.com/v1/search.php?key=${KEY}&q=${city}&format=json
+    //         `).then(data => {
+    //     let jObj = data.body[0];
+    //     let locObj = new Location(city, jObj.display_name, jObj.lat, jObj.lon);
+
+    let query = 'INSERT INTO location (search_query,formated_query,latitude,longitude) VALUE ($1,$2,$3,$4) RETURN *;';
+    console.log('');
+    client
+
+
+    //      res.status(200).json(locObj);
+    // }).catch((err) => {
+    //     res.send('location error...', err)
+    // });
+
+};
 
 //     let jData = require('./data/location.json');
 //     let jObj = jData[0];
@@ -39,6 +68,14 @@ function handleLocation(req, res) {
 // }
 //};
 
+function database(city) {
+    let query = 'SELECT * FROM location WHERE search_query = $1';
+    let value = [city];
+    return client.query(query, value).then(data => {
+        //console.log(data);
+        return data;
+    });
+}
 
 function Location(search_query, formatted_query, latitude, longitude) {
     this.search_query = search_query;
@@ -46,6 +83,8 @@ function Location(search_query, formatted_query, latitude, longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
 }
+
+*/
 
 // {
 //     "search_query": "seattle",
@@ -58,12 +97,13 @@ function handleWeather(req, res) {
     let city = req.query.search_query;
 
 
-    superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${KEY1}`)
+
+    superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city0}&key=${KEY1}`)
         .then(data => {
             let jObj = data.body.data;
-            console.log(jObj);
+            // console.log(jObj);
             let weatherArr = jObj.map((ele) => {
-                console.log(ele);
+                // console.log(ele);
                 let descript = ele.weather.description;
                 let date = transDate(Date.parse(ele.valid_date));
                 let locObj = new weather(descript, date, city);
@@ -76,8 +116,8 @@ function handleWeather(req, res) {
 
             res.status(200).json(weatherArr);
 
-        }).catch(() => {
-            res.send('an error');
+        }).catch((err) => {
+            res.send('an error', err);
         });
 }
 // try {
@@ -126,11 +166,11 @@ function transDate(value) {
 //     ....
 //   ]
 function handleTrails(req, res) {
-    //    let lat = req.query.latitude;
-    //  let lon = req.query.longitude;
-    // console.log('herer', lat, lon);
-    console.log('enter the fun');
-    superagent.get(`https://www.hikingproject.com/data/get-trails?lat=50&lon=50&maxDistance=5000&key=${KEY2}`)
+    let lat = req.query.latitude;
+    let lon = req.query.longitude;
+    console.log('herer', lat, lon);
+
+    superagent.get(`https://www.hikingproject.com/data/get-trails?lat= &lon= &maxDistance=2000&key=${KEY2}`)
         .then((data) => {
             console.log('herer', req.query.latitude, req.query.longitude);
             let jObj = data.body.trails;
@@ -144,8 +184,9 @@ function handleTrails(req, res) {
             });
             res.status(200).send(trailsArr);
 
-        }).catch(() => {
-            res.status(500).send('Something went wrong');
+        }).catch((err) => {
+            //console.log('Something went wrong...', err);
+            res.status(500).send('Something went wrong...', err);
         });
 };
 
@@ -159,11 +200,15 @@ function Trails(trailsData) {
     this.summary = trailsData.summary;
     this.trails_url = trailsData.url;
     this.conditions = trailsData.conditionStatus;
-    // this.conditions_date = trailsData.conditionDate.toString().slice(0, 10);
-    //this.conditions_time = trailsData.conditionDate.toString().slice(11, 20);
+    this.conditions_date = trailsData.conditionDate.toString().slice(0, 10);
+    this.conditions_time = trailsData.conditionDate.toString().slice(11, 20);
 
 }
+client.connect().then(() => {
 
-app.listen(PORT, () => {
-    console.log(PORT);
+    app.listen(PORT, () => {
+        console.log('the port is :', PORT);
+    });
+}).catch(err => {
+    console.log('there is an error in connection...', err)
 });
